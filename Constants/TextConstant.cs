06dd61677be47adbecb755198dca0ad0447fef0b
Shahir <shahir.abd@gmail.com>
2020-05-19 10:35:32 +0800

    Add files via upload

---
+++ Constants/TextConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POMSample.Constants
+{
+    class TextConstant
+    {
+        public const string googleHomePageTitle = "Software Testing Solution | Testbits | Top Application & Software Testing";
+    }
+}
